e9e78fc8ace0cf2b7d53828368ac07adc63a2c3f
Erick-EEV <EEVERICKEEV@gmail.com>
2020-11-10 22:12:28 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static/about'
+
   get 'about', to: 'static#about' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
